f83a03af3203a5f7c017440bd5f5f908679be2fc
VmcMK <rafinha.yamamoto75@gmail.com>
2024-06-06 21:14:54 -0300

    commit first

---
+++ app/teste/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Teste(){
+    return(
+        <div>
+            <Link href={"./"}>Página Inicial</Link>
+            <p>Minha Página de Teste</p>
+        </div>
+    )
+    
+}
